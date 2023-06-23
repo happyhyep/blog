e2a81b4cf5ef5eb1afa8ee010ce4791b85c8245e
happyhyep <jhi2359@naver.com>
2023-06-23 19:32:49 +0900

    activity page

--- blogl/src/Pages/Awards.js
+++ blogl/src/Pages/Awards.js
@@ -7,6 +7,9 @@ export default function Awards(){
         <>
             <NavBar></NavBar>
             <div className="profile" style={{display:"flex", justifyContent: 'center', marginTop:'70px', marginBottom: '80px'}}>페이지 준비중입니다 ...</div>
+            <div className="profile" style={{display:"flex", justifyContent: 'center', marginTop:'20px'}}>2023 San Jose State University Enterpreneurship Award 최우수</div>
+            <div className="profile" style={{display:"flex", justifyContent: 'center', marginTop:'20px'}}>2022 경희대학교 소프트웨어융합대학 동아리대항전 최우수</div>
+            <div className="profile" style={{display:"flex", justifyContent: 'center', marginTop:'20px'}}>2021 대학생 교육기부 공모전 장작 더하기 장려상 </div>
             <Footer />
         </>
     )
