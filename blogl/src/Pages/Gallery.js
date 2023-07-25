f56beac34e40ff123dd7b838d25c2ce7d2768fa1
happyhyep <jhi2359@naver.com>
2023-07-25 14:24:33 +0900

    awards 페이지 구현

--- blogl/src/Pages/Gallery.js
+++ blogl/src/Pages/Gallery.js
@@ -1,4 +1,5 @@
 import React from "react";
+import styled from "styled-components";
 import NavBar from "../components/Common/NavBar";
 import Footer from "../components/Common/Footer";
 
@@ -7,8 +8,32 @@ export default function Gallery(){
         <>
             <NavBar></NavBar>
             <div style={{display:"flex", justifyContent: 'center', marginTop:'70px', marginBottom: '80px'}}>페이지 준비중입니다 ...</div>
+            <Box>페이지 준비중입니다.</Box>
             <Footer />
         </> 
     )
 }
  
+const Box = styled.div`
+    @property --gradient-angle {
+        syntax: "<angle>";
+        initial-value: 0deg;
+        inherits: false;
+    }
+
+    inset: -.5rem;
+    width: 100%;
+
+
+    background: conic-gradient(
+        from var(--gradient-angle),
+        #01C7B7,
+        #21BCD6,
+        #1779BB
+    );
+    animation: rotation 20s linear infinite;
+    @keyframes rotation{
+        0% { --gradient-angle: 0deg; }
+        100% { --gradient-angle: 360deg; }
+    }
+`
