f33c182b6e68bcd23eac2b9b57ab4d87abf1a1ec
happyhyep <jhi2359@naver.com>
2023-07-25 10:26:56 +0900

    font modify

--- blogl/src/styles/fonts/GlobalStyle.js
+++ blogl/src/styles/fonts/GlobalStyle.js
@@ -13,6 +13,8 @@ const GlobalStyle = createGlobalStyle`
     };
     @font-face {
         font-family: "redemption";
+        font-style: normal;
+        font-weight: normal;
         src: url("./redemption.ttf") format("truetype"),
         url("./redemption.woff2") format("woff2"),
         url("./redemption.woff") format("woff");
