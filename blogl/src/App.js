4a24b848e4a5cec1cf3cedc3e4ad7b0b3f7441ac
happyhyep <jhi2359@naver.com>
2023-07-25 12:21:29 +0900

    font modify

--- blogl/src/App.js
+++ blogl/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Router from '../src/components/Common/Router';
+import GlobalStyle from './styles/fonts/GlobalStyle';
 
 function App() {
   return (
     <>
+      <GlobalStyle />
       <Router></Router> 
       </>
   );
